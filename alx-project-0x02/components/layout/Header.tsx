ce8b0f732e142b156eff31814c25726f27eb2e44
Tsedenawit <tsedenawit3@gmail.com>
2025-07-25 23:41:47 -0400

    changed

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import Link from "next/link";
 const Header:React.FC =()=>{
     return(
         <div>
             <p>welcome page</p>
+            <link href="/home">Home</link>
+            <link href="/about">About</link>
         </div>
     )
 }
